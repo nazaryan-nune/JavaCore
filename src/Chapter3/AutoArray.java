495219df16c3ee0072bd3a0091c81cb2234c2c88
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-09 15:28:43 +0400

    Chapter 3, added example

---
+++ src/Chapter3/AutoArray.java
@@ -0,0 +1,9 @@
+package Chapter3;
+
+//Նախորդ ծրագրի`Array.java, կատարելագործված տարբերակը
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("Ապրիլ ամիսն ունի " + month_days[3] + " օր։");
+    }
+}
