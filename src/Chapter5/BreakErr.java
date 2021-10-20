1cc886a88d9ea9e6471dcb95dac33321bd9951e2
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-20 14:53:24 +0400

    Chapter 5, added example

--- src/Chapter5/BreakErr.java
+++ src/Chapter5/BreakErr.java
@@ -3,15 +3,15 @@ package Chapter5;
 //Այս ծրագիրը պարունակում է սխալ
 public class BreakErr {
     public static void main(String[] args) {
-        one:
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Անցում " + i + ": ");
-        }
-        for (int j = 0; j < 100; j++) {
-            if (j == 10) {
-                break one; //ՍԽԱԼ
-            }
-            System.out.print(j + " ");
-        }
+//        one:
+//        for (int i = 0; i < 3; i++) {
+//            System.out.print("Անցում " + i + ": ");
+//        }
+//        for (int j = 0; j < 100; j++) {
+//            if (j == 10) {
+//                break one; //ՍԽԱԼ
+//            }
+//            System.out.print(j + " ");
+//        }
     }
 }
