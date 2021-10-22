d91e228c09f9585c09eab3bae37ebe90f68fa008
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-22 20:18:11 +0400

    4-րդ առաջադրանքի լուծումը փոփոխված է

--- src/Homework/charutil/CharUtil.java
+++ src/Homework/charutil/CharUtil.java
@@ -38,13 +38,12 @@ public class CharUtil {
         if (bolola[bolola.length - 2] == 'l' && bolola[bolola.length - 1] == 'y') {
             System.out.println("true" + " `զանգվածի վերջին տարրերն են ly" + '\n');
         } else {
-            System.out.println("false" + "      //ly -- զանգվածի նախավերջին և վերջին տարրերը չեն:" + '\n');
+            System.out.println("false" + "      //ly -- զանգվածի վերջին տարրերը չեն:" + '\n');
         }
 
         //4)Ունենք հետևյալ մասիվը՝
         //char[] bolola = {'b','a',b','o','l','a'};
         ////Տպում ենք true եթե տեքստը պարունակում է bob բառը, բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։
-
         char[] bolol = {'b', 'a', 'b', 'o', 'l', 'a' };
         boolean arraySymbolsContains = false;
         for (int i = 0; i < bolol.length; i++) {
@@ -53,17 +52,13 @@ public class CharUtil {
                 break;
             }
         }
-        System.out.println(arraySymbolsContains + "      //զանգվածը <<bob>> բառը պարունակում է" + "\n");
+        System.out.println(arraySymbolsContains);
 
-        //5)
+        //5)Ունենք հետևյալ մասիվը՝
+        //char[] text = {' ',' ','b','a','r','e','v',' ',' '};
+        //Տվյալ օրինակում կարող ենք կամայական քանակի պռաբելներ դնել թե սկզբից թե վերջից չկենտրոնանաք որ երկու հատ է դրված։
+        // Պետք է գրել կոդ, որը text մասիվից կստանա char[] result մասիվ, որը կլինի նույն տեքստը, առանց պռաբելների(իրար կողք կտպի barev)։
         char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
-        char[] result = new char[text.length];
 
-        for (int i = 0; i < text.length; i++) {
-            if (text[i] == 'b' || text[i] == 'a' || text[i] == 'r' || text[i] == 'e' || text[i] == 'v') ;
-            result[i] = text[i];
-        }
-        System.out.print("result զանգվածի տարրերն են՝ ");
-        System.out.println(result);
     }
 }
