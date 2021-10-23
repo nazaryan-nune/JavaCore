c97b42ee31ace9f9c18ef287d3c4d0e2fa5b4f36
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-23 23:46:39 +0400

    Initial commit

--- src/Chapter5/Return.java
+++ src/Chapter5/Return.java
@@ -3,6 +3,9 @@ package Chapter5;
 //Ցուցադրել return օպերատորի կիրառությունը
 public class Return {
     public static void main(String[] args) {
-
+        boolean t = true;
+        System.out.println("Дo возврата.");
+        if (t) return; // возврат в вызывающий код
+        System.out.println("Этот оператор выполняться не будет.");
     }
 }
