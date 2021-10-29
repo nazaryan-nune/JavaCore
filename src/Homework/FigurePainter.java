158f74d7fb3a001f4603a02928c1bb18d87e61ad
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-29 22:50:26 +0400

    Initial commit

--- src/Homework/FigurePainter.java
+++ src/Homework/FigurePainter.java
@@ -2,99 +2,99 @@ package Homework;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        /*
-         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
-         *
-         *         *
-         *         * *
-         *         * * *
-         *         * * * *
-         *
-         * */
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-
-        /*
-         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
-         *
-         *               *
-         *             * *
-         *           * * *
-         *         * * * *
-         *
-         */
+//        /*
+//         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+//         *
+//         *         *
+//         *         * *
+//         *         * * *
+//         *         * * * *
+//         *
+//         * */
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        /*
+//         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+//         *
+//         *               *
+//         *             * *
+//         *           * * *
+//         *         * * * *
+//         *
+//         */
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        /*
+//         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+//         *
+//         *         * * * *
+//         *         * * *
+//         *         * *
+//         *         *
+//         *
+//         */
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        /*
+//         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+//         *
+//         *         * * * *
+//         *           * * *
+//         *             * *
+//         *               *
+//         *
+//         */
+//
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        /*
+//         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+//         *
+//         *            *
+//         *           * *
+//         *          * * *
+//         *         * * * *
+//         *          * * *
+//         *           * *
+//         *            *
+//         *
+//         */
 
         for (int i = 0; i < 4; i++) {
             for (int j = 4; j > i; j--) {
-                System.out.print("  ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-
-        /*
-         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
-         *
-         *         * * * *
-         *         * * *
-         *         * *
-         *         *
-         *
-         */
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 4; j > i; j--) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-
-        /*
-         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
-         *
-         *         * * * *
-         *           * * *
-         *             * *
-         *               *
-         *
-         */
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("  ");
-            }
-            for (int j = 4; j > i; j--) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-
-        /*
-         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
-         *
-         *            *
-         *           * *
-         *          * * *
-         *         * * * *
-         *          * * *
-         *           * *
-         *            *
-         *
-         */
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 3; j > i; j--) {
                 System.out.print(" ");
             }
             for (int j = 0; j <= i; j++) {
@@ -102,7 +102,7 @@ public class FigurePainter {
             }
             System.out.println();
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(" ");
             }
