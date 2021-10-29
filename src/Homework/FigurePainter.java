3144c29d42d67d3fa2b004835e1fbb8dfcd1730a
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-10-29 22:05:34 +0400

    Initial commit

--- src/Homework/FigurePainter.java
+++ src/Homework/FigurePainter.java
@@ -1,65 +1,113 @@
 package Homework;
 
 public class FigurePainter {
-    //Առաջադրանք առաջին
     public static void main(String[] args) {
-        for (int a = 0; a < 4; a++) {
-            for (int b = 0; b <= a; b++) {
+        /*
+         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *         *
+         *         * *
+         *         * * *
+         *         * * * *
+         *
+         * */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        System.out.println();
 
-        //Առաջադրանք երկրորդ
-        for (int c = 0; c < 4; c++) {
-            for (int d = 0; d < 3 - c; d++) {
+
+        /*
+         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *               *
+         *             * *
+         *           * * *
+         *         * * * *
+         *
+         */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
                 System.out.print("  ");
             }
-            for (int e = 0; e <= c; e++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        /*
+         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *         * * * *
+         *         * * *
+         *         * *
+         *         *
+         *
+         */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        /*
+         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *         * * * *
+         *           * * *
+         *             * *
+         *               *
+         *
+         */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        /*
+         *   Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *            *
+         *           * *
+         *          * * *
+         *         * * * *
+         *          * * *
+         *           * *
+         *            *
+         *
+         */
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 3; j > i; j--) {
                 System.out.print(" *");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        //Առաջադրանք երրորդ
-        for (int f = 0; f < 4; f++) {
-            for (int g = 0; g < 4 - f; g++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        //Առաջադրանք չորրորդ
-        for (int h = 0; h < 4; h++) {
-            for (int i = 0; i < h; i++) {
-                System.out.print("  ");
-            }
-            for (int j = 0; j < 4 - h; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        //Առաջադրանք հինգերորդ
-        for (int k = 0; k < 3; k++) {
-            for (int l = 0; l < 3-k; l++) {
-                System.out.print(" ");
-            }
-            for (int m = 0; m <= k; m++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        for (int h = 0; h < 4; h++) {
-            for (int i = 0; i < h; i++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < 4 - h; j++) {
-                System.out.print("* ");
             }
             System.out.println();
         }
