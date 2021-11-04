01450014c4a2d90a0f7c8b8e68276bdc6f9d8dc5
Nune Nazaryan <jp.nazaryan@gmail.com>
2021-11-05 00:31:35 +0400

    Initial commit

--- src/homework/practiceHomework/PracticeHomework.java
+++ src/homework/practiceHomework/PracticeHomework.java
@@ -14,28 +14,18 @@ public class PracticeHomework {
 
     //վերադարձնել տրված թվի հաջորդ թիվը
     int nextNumber(int number) {
-        return number + 1;
+        return ++number;
     }
 
     //վերադարձնել true եթե տրված a և b թվերը իրար հավասար են, եթե ոչ՝ false
-    boolean result = false;
-
     boolean isSameNum(int a, int b) {
-        if (a == b) {
-            result = true;
-        }
-        return result;
+        return a == b;
     }
 
 
     //վերադարձնել true-եթե թիվը փոքր կամ հավասար է 0-ին։ Մնացած դեպքերում վերադարձնել false
-    boolean found = false;
-
     boolean lessThanOrEqualToZero(int number2) {
-        if (number2 <= 0) {
-            found = true;
-        }
-        return found;
+        return number2 <= 0;
     }
 
 
@@ -47,10 +37,6 @@ public class PracticeHomework {
 
     //վերադարձնել երկու մասիվի ամենամեծ length-ը
     int maxLength(int[] array1, int[] array2) {
-        int arrayLength = array2.length;
-        if (array1.length > array2.length) {
-            arrayLength = array1.length;
-        }
-        return arrayLength;
+        return array1.length > array2.length ? array1.length : array2.length;
     }
 }
--- src/homework/practiceHomework/PracticeHomeworkTest.java
+++ src/homework/practiceHomework/PracticeHomeworkTest.java
@@ -3,22 +3,20 @@ package homework.practiceHomework;
 public class PracticeHomeworkTest {
     public static void main(String[] args) {
         PracticeHomework homework = new PracticeHomework();
-        //փոխակերպել տրված րոպեն վայրկյանի
 
+        //փոխակերպել տրված րոպեն վայրկյանի
         System.out.println(15 + " րոպեն " + homework.convert(15) + " վայրկյան է։");
 
 
-//ընդունել տարին, վերադարձնել օրերի քանակը
-
+        //ընդունել տարին, վերադարձնել օրերի քանակը
         System.out.println(7 + " տարին " + homework.calcAge(7) + " օր է։");
 
 
-//վերադարձնել տրված թվի հաջորդ թիվը
-
+        //վերադարձնել տրված թվի հաջորդ թիվը
         System.out.println(33 + "-ի հաջորդ թիվը " + homework.nextNumber(33) + "-ն է։");
 
 
-//վերադարձնել true եթե տրված a և b թվերը իրար հավասար են, եթե ոչ՝ false
+        //վերադարձնել true եթե տրված a և b թվերը իրար հավասար են, եթե ոչ՝ false
 
         if (homework.isSameNum(15, 27)) {
             System.out.println(homework.isSameNum(15, 27) + "     քանի որ a=" + 15 + " և b=" + 27 + " թվերը " + "հավասար են։");
@@ -27,8 +25,8 @@ public class PracticeHomeworkTest {
         }
 
 
-//վերադարձնել true - եթե թիվը փոքր կամ հավասար է 0 - ին։
-//մնացած դեպքերում վերադարձնել false
+        //վերադարձնել true - եթե թիվը փոքր կամ հավասար է 0 - ին։
+        //մնացած դեպքերում վերադարձնել false
 
         if (homework.lessThanOrEqualToZero(-15)) {
             System.out.println(homework.lessThanOrEqualToZero(-15) + "      քանի որ " + -15 + "<0:");//==??
@@ -37,7 +35,7 @@ public class PracticeHomeworkTest {
         }
 
 
-//վերադարձնել ընդունող բուլյանի ժխտված արժեքը
+        //վերադարձնել ընդունող բուլյանի ժխտված արժեքը
 
         if (homework.reverseBool(false)) {
             System.out.println(homework.reverseBool(false) + "      քանի որ false-ի ժխտված արժեքը true-ն է։");
@@ -46,9 +44,11 @@ public class PracticeHomeworkTest {
         }
 
 
-//վերադարձնել երկու մասիվի ամենամեծ length-ը
+        //վերադարձնել երկու մասիվի ամենամեծ length-ը
+
         int[] array1 = {10, 5, 71, 21, 1, 50, 7, 1, 99, 100, 5, 71, 21, 1};
         int[] array2 = {110, 5, 71, 21, 12, 57, 5, 15, 99, 100};
+
         if (homework.maxLength(array1, array2) == array1.length) {
             System.out.println(homework.maxLength(array1, array2) + "        I զանգվածի երկարությունը գերազանցում է II զանգվածի երկարությունը։");
         } else {
